a094aa02ec682defbcae01ead3b68d75f1511df5
unknown <ahmdfauzanrr@gmail.com>
2025-05-26 21:32:02 +0800

    First deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Import plugin
 
 module.exports = {
   entry: './src/app.js',
@@ -21,4 +22,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Gunakan template HTML Anda
+    }),
+  ],
 };
